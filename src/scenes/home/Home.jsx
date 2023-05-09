cba58e29bc9f5c569ed6a3de4478bf43251c9858
Carlos Gil <carlos.gil@chalmers.se>
2023-05-09 20:35:33 +0200

    created Item component

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,4 +1,11 @@
-
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, useTheme, Typography, Button } from "@mui/material";
+import AddIcon from "@mui/material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../theme";
+import { addToCart } from "../state";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
     return <div>Home</div>
